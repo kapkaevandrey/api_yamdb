3e87baf569afdc1a957e6c043dfd452164cccf9a
Andrey Kapkaev <kapkaew@yandex.ru>
2021-10-13 01:31:07 +0300

    Зарегистрированы все url приложения api и эндопоинты для моделей Review и Comments.
    Прописаны эндпоинты для моделе Review и Comments через регулярные выражения.

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import CommentViewSet, ReviewViewSet
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'title/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename="reviews")
+router_v1.register(r'title/(?P<title_id>\d+)/(?P<review_id>\d+)/comments',
+                   CommentViewSet, basename="comments")
+
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls))
+]
