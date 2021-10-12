f5f3a6948c786891713a79920e5cdb47c5ccfc0f
Andrey Kapkaev <kapkaew@yandex.ru>
2021-10-13 00:32:48 +0300

    Добавлена модель Review.
    Создана модель Review, которая включает в себя отзывы на произведения (Title) дату создания и рейтинг выбираемый в диапазоне 1-10, в модели учитвается уникальность отзывов автора на конкретное произведение.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    text = models.TextField()
+    title = models.IntegerField() # TODO заменить на FK когда модель Title будет написана
+    pub_date = models.DateTimeField('Date of publication',
+                                    auto_now_add=True,
+                                    db_index=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="reviews")
+    score = models.IntegerField(default=1,
+                                validators=[
+                                    MaxValueValidator(10),
+                                    MinValueValidator(1)
+                                ])
+
+    def __str__(self):
+        return (f'Author - <{self.author}>; title - <{self.title}>'
+                f'text part - <{self.text[:15]}>.')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['author', 'title'],
+                                    name='unique review'),
+        ]
+        ordering = ['-pub_date']
+
+
+
