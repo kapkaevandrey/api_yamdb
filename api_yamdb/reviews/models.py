bfe794aa2bdddb61a9cec1500092f30d0009adf1
Andrey Kapkaev <kapkaew@yandex.ru>
2021-10-13 00:41:24 +0300

    Добавлена модель Comments.
    Модель сохраняет комментарии к отзывам на произведения.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,23 +2,22 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-
 User = get_user_model()
 
 
 class Review(models.Model):
     text = models.TextField()
-    title = models.IntegerField() # TODO заменить на FK когда модель Title будет написана
     pub_date = models.DateTimeField('Date of publication',
                                     auto_now_add=True,
                                     db_index=True)
+    score = models.PositiveSmallIntegerField(default=1,
+                                             validators=[
+                                                 MaxValueValidator(10),
+                                                 MinValueValidator(1)
+                                             ])
+    title = models.IntegerField()  # TODO заменить на FK когда модель Title будет написана
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="reviews")
-    score = models.IntegerField(default=1,
-                                validators=[
-                                    MaxValueValidator(10),
-                                    MinValueValidator(1)
-                                ])
 
     def __str__(self):
         return (f'Author - <{self.author}>; title - <{self.title}>'
@@ -32,4 +31,13 @@ class Review(models.Model):
         ordering = ['-pub_date']
 
 
+class Comment(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Date of publication',
+                                    auto_now_add=True,
+                                    db_index=True)
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
 
+    class Meta:
+        ordering = ['-pub_date']
