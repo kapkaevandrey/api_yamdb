6304b1b30f050bf68483739d4ed2db40e664f4a1
Andrey Kapkaev <kapkaew@yandex.ru>
2021-10-14 00:20:30 +0300

    Рефакторинг и переимнование моделей и некоторых полей.
    Привёл в порядок наборы импортов. Для каждой модели прописано строковое представление.
    У каждой модели есть порядок сортировки по умолчанию. Некоторы поля частично измененены, некоторые IntegerFields были заменены на PositiveSmallIntegerFields.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,41 +1,81 @@
-from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, MaxValueValidator, validate_slug
-from django.db import models
+from datetime import datetime
 
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.core.validators import (MinValueValidator,
+                                    MaxValueValidator,
+                                    validate_slug)
+
+
+CURRENT_YEAR = datetime.now().year
 
 User = get_user_model()
 
 
-class Categories(models.Model):
+class Category(models.Model):
+    """Модель категорий произведений."""
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True, validators=[validate_slug])
 
+    def __str__(self):
+        return f"genre_id - <{self.id}, slug - <{self.slug}>"
 
-class Genres(models.Model):
+    class Meta:
+        ordering = ("name",)
+
+
+class Genre(models.Model):
+    """Модель жанров произведений."""
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, validators=[validate_slug])
+
+    def __str__(self):
+        return f"genre_id - <{self.id}, slug - <{self.slug}>"
+
+    class Meta:
+        ordering = ("name",)
 
 
 class Titles(models.Model):
+    """Модель произведений."""
     name = models.CharField(max_length=50)
-    year = models.IntegerField(validators=[MaxValueValidator(2022)])
-    rating = models.IntegerField(blank=True, null=True)
+    year = models.PositiveSmallIntegerField(validators=[MaxValueValidator(CURRENT_YEAR + 1)])
+    rating = models.PositiveSmallIntegerField(default=0,
+                                              validators=[MaxValueValidator(10)])
     description = models.TextField(blank=True)
-    genry = models.ManyToManyField(Genres)
-    category = models.ForeignKey(Categories, on_delete=SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre, through="GenreTitle", blank=True, null=True)
+
+    def __str__(self):
+        return f"title_id - <{self.id}, name - <{self.name[:15]}>"
+
+    class Meta:
+        ordering = ['category__name']
+
+
+class GenreTitle(models.Model):
+    """Модель связи жанра и произведения."""
+    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"title_id - <{self.title.id}> <{self.title.name[:15]}>"
+                f"gener_id - <{self.genre_id}> <{self.genre.slug}>")
 
 
 class Review(models.Model):
+    """Модель отзыва на произведение."""
     text = models.TextField()
     pub_date = models.DateTimeField('Date of publication',
                                     auto_now_add=True,
                                     db_index=True)
     score = models.PositiveSmallIntegerField(default=1,
                                              validators=[
-                                                 MaxValueValidator(10),
-                                                 MinValueValidator(1)
+                                                 MinValueValidator(1),
+                                                 MaxValueValidator(10)
                                              ])
-    title = models.IntegerField()  # TODO заменить на FK когда модель Title будет написана
+    title = models.ForeignKey(Titles, on_delete=models.CASCADE,
+                              related_name="reviews")
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="reviews")
 
@@ -52,6 +92,7 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
+    """Модель комментария к отзыву."""
     text = models.TextField()
     pub_date = models.DateTimeField('Date of publication',
                                     auto_now_add=True,
