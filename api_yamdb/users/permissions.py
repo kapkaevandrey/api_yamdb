7e40a5e19d9c066cb7eaf01d6801c6d8b325f14d
Tony <elkobrat@protonmail.com>
2021-10-18 20:31:17 +1000

    user permissions updated

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,11 +1,13 @@
 from rest_framework import permissions
 
-from .models import User
+from .models import Role
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_superuser
-            or request.user.role == User.ADMIN
-        )
+        if request.user.is_authenticated:
+            return (request.user.is_superuser
+                    or request.user.is_staff
+                    or request.user.role == Role.ADMIN
+                    )
+        return False
