096bb500828e4badce0301b980de5f0d2bf1cd97
Andrey Kapkaev <kapkaew@yandex.ru>
2021-10-18 18:36:29 +0300

    Внесены правка в permission, serializer, views Приложения API

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,6 +3,7 @@ from rest_framework import permissions
 
 class CategoryGenryTitlePermissions(permissions.BasePermission):
     """Фильтр - разрешает GET запросы всем, остальное только для ADMIN"""
+
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
@@ -13,10 +14,14 @@ class AuthorAdminModeratorOrReadOnly(permissions.BasePermission):
     """Разрешение на чтение для всех пользователей.
     Редактирование, обновление или удаление разрешено только
     авторам, модераторам и администраторам."""
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         object_author = obj.author == request.user
-        admin_or_moderator = (getattr(request.user, "role", "user") in
-                              ['admin', 'moderator'])
-        return object_author or admin_or_moderator
+        admin_or_moderator = (
+                request.user.is_authenticated
+                and (request.user.role == request.user.ADMIN
+                     or request.user.role == request.user.MODERATOR))
+        return object_author or admin_or_moderator or request.user.is_staff or request.user.is_superuser
+    # TODO вынести определение свойства в отдельный метод
