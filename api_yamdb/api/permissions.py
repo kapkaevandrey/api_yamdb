506d78177cb9908a977715739e9ed24f4cd36ab7
Andrey Kapkaev <kapkaew@yandex.ru>
2021-10-20 11:24:34 +0300

    Fix style permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -16,16 +16,13 @@ class AuthorAdminModeratorOrReadOnly(permissions.BasePermission):
     авторам, модераторам и администраторам."""
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return (obj.author == request.user
-                    or request.user.is_admin_or_moderator)
-        return False
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and (obj.author == request.user
+                         or request.user.is_admin_or_moderator)))
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.is_admin
-        return False
+        return (request.user.is_authenticated
+                and request.user.is_admin)
